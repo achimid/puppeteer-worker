9c7260802a5349a3f32c3b72a06da2ad1762256b
Lourran <achimid@hotmail.com>
2023-07-05 19:19:32 -0300

    ajustando

--- src/index.js
+++ src/index.js
@@ -4,16 +4,4 @@ const browserInit = require('./puppeteer')
 const { initConsumer } = require('./execution-service')
 
 
-const { sendToQueue, consumeFromQueue } = require('./queue')
-
-
-setInterval(() => {
-    sendToQueue('WORKER_EXECUTION_REQUEST', require('../request.json'))
-}, 6000)
-
-// consumeFromQueue('WORKER_EXECUTION_RESPONSE', (data, ack) => {
-//     console.log(JSON.parse(data.content.toString()))
-//     ack()
-// })
-
 browserInit().then(initConsumer)
